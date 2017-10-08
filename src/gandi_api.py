994061af1e401ad543285eddbb066f8b635478f5
Luan Guimarães <luang@protonmail.ch>
2017-10-07 23:24:51 -0300

    Creating GandiHandler class to help with Gandi's API

---
+++ src/gandi_api.py
@@ -0,0 +1,67 @@
+import requests
+
+class GandiHandler:
+    def __init__(self, api, api_key):
+        self.api = api
+        self.api_key = api_key
+        self.zones_info = self.get_zones_information()
+
+    def get_zones_information(self):
+        request_headers = {"X-Api-Key": self.api_key}
+        http_response = requests.get(self.api + "zones", headers=request_headers)
+        return http_response.json()
+
+    def _get_zone_info(self, attribute, domain):
+        for zone in self.zones_info:
+            if zone['name'].find(domain) >= 0: #if domain matches
+                return zone[attribute]
+        else:
+            return None
+
+    def get_zone_uuid(self, domain):
+        return self._get_zone_info('uuid', domain)
+
+    def get_zone_href(self, domain):
+        return self._get_zone_info('zone_href', domain)
+
+    def get_zone_records(self, domain):
+        request_headers = {"X-Api-Key": self.api_key}
+        zone_href = self.get_zone_href(domain)
+        http_response = requests.get(zone_href + '/records', headers=request_headers)
+        zone_records = http_response.json()
+        return zone_records
+
+    def _get_record_info(self, attribute, record_name, record_type, domain):
+        request_headers = {"X-Api-Key": self.api_key}
+        zone_records = self.get_zone_records(domain)
+        for record in zone_records:
+            if record['rrset_name'] == record_name and record['rrset_type'] == record_type:
+                return record[attribute]
+        else:
+            return None
+
+    def get_record_href(self, record_name, record_type, domain):
+        return self._get_record_info('rrset_href', record_name, record_type, domain)
+
+    def change_zone_a_record(self, new_ip, record_name, ttl, domain):
+        request_headers = {
+            "Content-Type": "application/json",
+            "X-Api-Key": self.api_key
+        }
+
+        request_data = {
+            "rrset_ttl": ttl,
+            "rrset_values": [
+                new_ip
+            ]
+        }
+
+        record_href = self.get_record_href(record_name, "A", domain) 
+        print("Recording new IP(" + new_ip + ") for " + record_name + ".")
+        print("URL: " + record_href)
+
+        http_response = requests.put(record_href,
+            headers=request_headers, data=str(request_data).replace("'",'"'))
+
+        print("Request response status: " + str(http_response.status_code))
+        print("Request text: " + http_response.text)
